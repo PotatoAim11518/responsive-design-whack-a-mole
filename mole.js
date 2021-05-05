a760f5dc762efc1585cbcbc6535d15c1c7475841
wilson <whuang3015@gmail.com>
2021-05-05 16:35:54 -0700

    Implement CHAOS

--- mole.js
+++ mole.js
@@ -2,25 +2,31 @@ function popUpRandomMole() {
   let moleHeads = document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)");
   let randomNum = Math.floor(Math.random() * moleHeads.length);
   let mole = moleHeads[randomNum];
-  if (moleHeads.length === 0) window.alert('YOU WON!!!');
+  if (counter === 20) {
+    counter = 0;
+    window.alert('YOU WON!!!');
+    location.reload()
+  }
   mole.classList.remove('wgs__mole-head--hidden');
-  setTimeout(()=> hideMole(mole), 1000);
+  setTimeout(()=> hideMole(mole), 5000);
 }
 
 function hideMole(mole) {
   mole.classList.add('wgs__mole-head--hidden');
   setTimeout(popUpRandomMole, 0);
 }
+let counter = 0;
 
 window.addEventListener('DOMContentLoaded', () => {
   setTimeout(popUpRandomMole, 0);
   let moleHeads = document.querySelectorAll(".wgs__mole-head");
   moleHeads.forEach( mole => {
     mole.addEventListener('click', (event)=> {
+      ++counter;
       // console.log('BONK')
       event.target.classList.add('wgs__mole-head--hidden');
-      event.target.classList.add('wgs__mole-head--whacked');
-      // popUpRandomMole();
+      // event.target.classList.add('wgs__mole-head--whacked');
+      popUpRandomMole();
     });
   });
 });
