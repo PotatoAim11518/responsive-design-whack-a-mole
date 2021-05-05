2579b7b51e748cdf3d1643ce59b13335017350f4
wilson <whuang3015@gmail.com>
2021-05-05 11:37:26 -0700

    Implemented random diglett hiding

--- mole.js
+++ mole.js
@@ -1,16 +1,28 @@
 // Write your JS here.
 // window.addEventListener('DOMContentLoaded', () => {
 
-//   setInterval(() => {
-//     const moleHeads = document.querySelectorAll('.wgs__mole-head');
-//     for (let moleHead of moleHeads) {
-//       moleHead.classList.toggle('wgs__mole-head--hidden');
-//     }
-//   }, 1000);
+  //   setInterval(() => {
+    //     const moleHeads = document.querySelectorAll('.wgs__mole-head');
+    //     for (let moleHead of moleHeads) {
+      //       moleHead.classList.toggle('wgs__mole-head--hidden');
+      //     }
+      //   }, 1000);
 
-// });
+      // });
+
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(popUpRandomMole, 0);
+});
 
 function popUpRandomMole() {
   let moleHeads = document.querySelectorAll(".wgs__mole-head");
   let randomNum = Math.floor(Math.random() * 8);
+  let mole = moleHeads[randomNum];
+  mole.classList.remove('wgs__mole-head--hidden');
+  setTimeout(hideMole, 1000, mole);
+}
+
+function hideMole(mole) {
+  mole.classList.add('wgs__mole-head--hidden');
+  setTimeout(popUpRandomMole, 1000);
 }
