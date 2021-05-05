85a2d0cef536a94858bf6a4bbe079767bd730ca0
wilson <whuang3015@gmail.com>
2021-05-05 10:41:28 -0700

    Complete Step1 and begin Step2

--- mole.js
+++ mole.js
@@ -1,1 +1,16 @@
 // Write your JS here.
+// window.addEventListener('DOMContentLoaded', () => {
+
+//   setInterval(() => {
+//     const moleHeads = document.querySelectorAll('.wgs__mole-head');
+//     for (let moleHead of moleHeads) {
+//       moleHead.classList.toggle('wgs__mole-head--hidden');
+//     }
+//   }, 1000);
+
+// });
+
+function popUpRandomMole() {
+  let moleHeads = document.querySelectorAll(".wgs__mole-head");
+  let randomNum = Math.floor(Math.random() * 8);
+}
