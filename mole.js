8ea079db62e36e9bfe1141934afe42ed0cc95e9c
wilson <whuang3015@gmail.com>
2021-05-05 17:22:05 -0700

    Add counter

--- mole.js
+++ mole.js
@@ -2,8 +2,8 @@ function popUpRandomMole() {
   let moleHeads = document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)");
   let randomNum = Math.floor(Math.random() * moleHeads.length);
   let mole = moleHeads[randomNum];
-  if (counter === 20) {
-    counter = 0;
+  if (counter === 0) {
+    counter = 30;
     window.alert('YOU WON!!!');
     location.reload()
   }
@@ -15,15 +15,17 @@ function hideMole(mole) {
   mole.classList.add('wgs__mole-head--hidden');
   setTimeout(popUpRandomMole, 0);
 }
-let counter = 0;
+let counter = 30;
 
 window.addEventListener('DOMContentLoaded', () => {
   setTimeout(popUpRandomMole, 0);
   let moleHeads = document.querySelectorAll(".wgs__mole-head");
+  let toBonk = document.getElementById('counter');
+  toBonk.innerHTML = `Digletts left to bonk: ${counter}`;
   moleHeads.forEach( mole => {
     mole.addEventListener('click', (event)=> {
-      ++counter;
-      // console.log('BONK')
+      --counter;
+      toBonk.innerHTML = `Digletts left to bonk: ${counter}`;
       event.target.classList.add('wgs__mole-head--hidden');
       // event.target.classList.add('wgs__mole-head--whacked');
       popUpRandomMole();
